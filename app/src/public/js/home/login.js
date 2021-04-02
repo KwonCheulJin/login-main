6f8adbb46828edda05646d29ce2065af6462b830
KwonCheulJin <KwonCheulJin@users.noreply.github.com>
2021-04-02 11:59:38 +0900

    DOM이용 HTML객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -2,4 +2,15 @@
 
 const id = document.querySelector('#id'),
   pw = document.querySelector("#pw"),
-  login = document.querySelector('button');
+  loginBtn = document.querySelector('button');
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.value,
+    pw: pw.value
+  };
+  console.log(req);
+  // fetch();
+}
